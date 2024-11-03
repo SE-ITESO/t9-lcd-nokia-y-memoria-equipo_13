6ecf101ea5a0740085ed962964f8705790b334e8
Rodrigo-Ramos-R <ie715082@iteso.mx>
2024-11-03 17:35:52 -0600

    SPI funcional

--- SPI.c
+++ SPI.c
@@ -26,9 +26,13 @@
 
 /* TODO: insert other definitions and declarations here. */
 extern uint8_t g_master_rxBuffImage_1[IMAGE_MEMORY_SIZE];
+extern uint8_t g_master_rxBuffImage_2[IMAGE_MEMORY_SIZE];
+extern uint8_t g_master_rxBuffImage_3[IMAGE_MEMORY_SIZE];
+extern uint8_t g_master_rxBuffImage_4[IMAGE_MEMORY_SIZE];
+extern uint8_t g_master_rxBuffImage_5[IMAGE_MEMORY_SIZE];
 
-
-void SPI_config(void){
+void SPI_config(void)
+{
 
 	uint32_t srcClock_Hz;
 
@@ -52,10 +56,103 @@ void SPI_config(void){
 	PORT_SetPinConfig(PORTD, PIN2_IDX, &button_config);
 	PORT_SetPinConfig(PORTD, PIN3_IDX, &button_config);
 
+	/*configuracion LCD */
+	dspi_master_config_t masterConfig;
 
+	masterConfig.whichCtar                                = kDSPI_Ctar0;
+	masterConfig.ctarConfig.baudRate                      = TRANSFER_BAUDRATE;
+	masterConfig.ctarConfig.bitsPerFrame                  = 8U;
+	masterConfig.ctarConfig.cpol                          = kDSPI_ClockPolarityActiveHigh;
+	masterConfig.ctarConfig.cpha                          = kDSPI_ClockPhaseFirstEdge;
+	masterConfig.ctarConfig.direction                     = kDSPI_MsbFirst;
+	masterConfig.ctarConfig.pcsToSckDelayInNanoSec        = 1000000000U / TRANSFER_BAUDRATE;
+	masterConfig.ctarConfig.lastSckToPcsDelayInNanoSec    = 1000000000U / TRANSFER_BAUDRATE;
+	masterConfig.ctarConfig.betweenTransferDelayInNanoSec = 1000000000U / TRANSFER_BAUDRATE;
+
+	masterConfig.whichPcs           = kDSPI_Pcs0;
+	masterConfig.pcsActiveHighOrLow = kDSPI_PcsActiveLow;
+
+	masterConfig.enableContinuousSCK        = false;
+	masterConfig.enableRxFifoOverWrite      = false; //Tienes FIFOS; recuibe mas de un byte a la vez, "quiero que me sobrescribas el valor mas alto o no"
+	masterConfig.enableModifiedTimingFormat = false;
+	masterConfig.samplePoint                = kDSPI_SckToSin0Clock;
+
+	srcClock_Hz = DSPI_MASTER_CLK_FREQ;
+	DSPI_MasterInit(EXAMPLE_DSPI_MASTER_BASEADDR, &masterConfig, srcClock_Hz);
+
+
+	/* Configuracion memoria */
+
+	masterConfig.whichCtar                                = kDSPI_Ctar1;
+	masterConfig.ctarConfig.baudRate                      = TRANSFER_BAUDRATE_MEMORY;
+	masterConfig.ctarConfig.bitsPerFrame                  = 8U;
+	masterConfig.ctarConfig.cpol                          = kDSPI_ClockPolarityActiveLow;
+	masterConfig.ctarConfig.cpha                          = kDSPI_ClockPhaseSecondEdge;
+	masterConfig.ctarConfig.direction                     = kDSPI_MsbFirst;
+	masterConfig.ctarConfig.pcsToSckDelayInNanoSec        = 1000000000U / TRANSFER_BAUDRATE_MEMORY;
+	masterConfig.ctarConfig.lastSckToPcsDelayInNanoSec    = 1000000000U / TRANSFER_BAUDRATE_MEMORY;
+	masterConfig.ctarConfig.betweenTransferDelayInNanoSec = 1000000000U / TRANSFER_BAUDRATE_MEMORY;
+
+	masterConfig.whichPcs           = kDSPI_Pcs1;
+	masterConfig.pcsActiveHighOrLow = kDSPI_PcsActiveLow;
+
+	masterConfig.enableContinuousSCK        = false;
+	masterConfig.enableRxFifoOverWrite      = false;
+	masterConfig.enableModifiedTimingFormat = false;
+	masterConfig.samplePoint                = kDSPI_SckToSin0Clock;
+
+	DSPI_MasterInit(EXAMPLE_DSPI_MASTER_BASEADDR, &masterConfig, srcClock_Hz);
 
 }
+
 void Recive_Data(uint8_t x){
+	dspi_half_duplex_transfer_t TransferMemory;
+
+	uint8_t g_master_txADDRImage_1[bit_4] = {0x03, 0x04, 0x00, 0x00};
+	uint8_t g_master_txADDRImage_2[bit_4] = {0x03, 0x04, 0x10, 0x00};
+	uint8_t g_master_txADDRImage_3[bit_4] = {0x03, 0x04, 0x20, 0x00};
+	uint8_t g_master_txADDRImage_4[bit_4] = {0x03, 0x04, 0x30, 0x00};
+	uint8_t g_master_txADDRImage_5[bit_4] = {0x03, 0x04, 0x40, 0x00};
+
+	switch(x){
+	case bit_1:
+		TransferMemory.txData					= g_master_txADDRImage_1;
+		TransferMemory.rxData					= g_master_rxBuffImage_1;
+		break;
+
+	case bit_2:
+		TransferMemory.txData					= g_master_txADDRImage_2;
+		TransferMemory.rxData					= g_master_rxBuffImage_2;
+		break;
+
+	case bit_3:
+		TransferMemory.txData					= g_master_txADDRImage_3;
+		TransferMemory.rxData					= g_master_rxBuffImage_3;
+		break;
+
+	case bit_4:
+		TransferMemory.txData					= g_master_txADDRImage_4;
+		TransferMemory.rxData					= g_master_rxBuffImage_4;
+		break;
+
+	case bit_5:
+		TransferMemory.txData					= g_master_txADDRImage_5;
+		TransferMemory.rxData					= g_master_rxBuffImage_5;
+		break;
+
+	default:
+		TransferMemory.txData					= g_master_txADDRImage_1;
+		TransferMemory.rxData					= g_master_rxBuffImage_1;
+		break;
+	}
+
+	TransferMemory.txDataSize				= bit_4;
+	TransferMemory.rxDataSize				= IMAGE_MEMORY_SIZE;
+	TransferMemory.isTransmitFirst			= true;
+	TransferMemory.isPcsAssertInTransfer	= true;
+	TransferMemory.configFlags				= kDSPI_MasterCtar1 | kDSPI_MasterPcs1 | kDSPI_MasterPcsContinuous;
+
+	DSPI_MasterHalfDuplexTransferBlocking(EXAMPLE_DSPI_MASTER_BASEADDR, &TransferMemory);
 
 }
 
--- SPI.h
+++ SPI.h
@@ -30,10 +30,34 @@
 #define EXAMPLE_DSPI_MASTER_PCS_FOR_TRANSFER kDSPI_MasterPcs0
 #define EXAMPLE_DSPI_DEALY_COUNT             0xfffffU
 
+/*Definiciones para la transferencia de datos, tamanos y baudrates */
+#define TRANSFER_SIZE     				64U     /*! Transfer dataSize */
+#define TRANSFER_BAUDRATE 				1000000U /*! Transfer baudrate - 1M */
+#define TRANSFER_BAUDRATE_MEMORY 		2000000U /*! Transfer baudrate - 2M */
+#define IMAGE_MEMORY_SIZE				504		/* Images have size of 504 bytes */
 
+/* Pines a usar para evitar magic numbers*/
+#define PIN0_IDX 	0u
+#define PIN1_IDX 	1u
+#define PIN2_IDX 	2u
+#define PIN3_IDX 	3u
+#define PIN16_IDX  	16u
+#define PIN17_IDX 	17u
 
+/*!
+ * \brief configuracion para hacer transferencias con SPI
+ *
+ * \param[in] void
+ * \return void
+ */
 void SPI_config(void);
 
+/*!
+ * \brief funcion para recibir datos con SPI
+ *
+ * \param[in] uint8_t
+ * \return void
+ */
 void Recive_Data(uint8_t x);
 
 #endif /* SPI_H_ */
