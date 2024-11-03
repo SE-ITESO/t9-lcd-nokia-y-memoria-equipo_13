95d2e396795ca23fd0a693030ca681e4a2a06d4a
Rodrigo-Ramos-R <ie715082@iteso.mx>
2024-11-03 00:21:37 -0600

    creacion del main

---
+++ main.c
@@ -0,0 +1,27 @@
+/*
+ * File
+ * 	main.c
+ *
+ * Brief
+ * 	main de la tarea 09, visualizacion de imagenes en pantalla LCD nokia
+ * 	leyendo las imagenes de una memoria S25FL164K
+ *
+ * Authors
+ *  Rodrigo Ramos Romero
+ *	Gerardo Padilla Torres
+ * Date
+ *  29/11/2024
+ * todo
+ *
+ */
+ 
+/* TODO: insert other include files here. */
+
+#include "bits.h"
+
+int main(void)
+{
+
+	return 0;
+}
+
